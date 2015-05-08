8505e99f6a67476beb3e95b190dec1bf23ce90ed
sjjung <easter0113@gmail.com>
2015-05-08 16:44:28 +0900

    Dong

--- src/main/webapp/js/list_view.js
+++ src/main/webapp/js/list_view.js
@@ -3,7 +3,7 @@
  */
 
 app.controller("listViewController", function($scope, $http, $location) {
-	$scope.id="1";
+
 	
    $http.get("qna/aaa").success(function(loginstatus) {
          $scope.articles = loginstatus;
@@ -13,10 +13,15 @@ app.controller("listViewController", function($scope, $http, $location) {
    });
    
    
-   
-   $scope.aaa = function() {
-	   $scope.$parent.pageClass = 'page-about';
-	$scope.parent.id=$scope.id;
+   $scope.showId = function(id) {
+//	   alert("id = " + id);
+
+	   $scope.$parent.contentID = id;
+	   
+	   $location.path("/show_content");
    };
-   
+      
 });
+
+
+
